0830e6b289686d4b52a52d577ba9fc85e4bfaeb4
Akshay K <akshaykunti1@gmail.com>
2020-01-05 11:33:05 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+  return true if num == 2
+  return false if num < 2
+  count = 2
+  is_prime = true
+  while count < num
+    if num % count == 0
+      is_prime = false
+    end
+    count += 1
+  end
+  return is_prime
+end
+  
